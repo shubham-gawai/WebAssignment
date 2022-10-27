ec8b98e098801c950610e11830ea0323f7aef473
Shubham Gawai <gawaishubham001@gmail.com>
2022-10-27 13:15:42 +0530

    402-02 done

---
+++ 4-Advance-JS/402-DOM-Manipulation/AdvanceJS-402-02/app.js
@@ -0,0 +1,16 @@
+
+// new elements created ouside the function coz of loop : now function only will be called once a onclick
+
+var c = document.createElement("img");
+var d = document.createElement("h3");
+
+function chnageimage(){
+    d.innerHTML="Image Element Added.";
+    
+    c.src = "https://geekster.in/images/logo.svg";
+
+    // Append to another element:
+    document.getElementById("new-div").appendChild(d);
+    document.getElementById("new-div").appendChild(c);
+    
+}
