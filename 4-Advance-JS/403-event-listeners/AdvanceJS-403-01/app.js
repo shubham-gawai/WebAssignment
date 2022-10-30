37f7f814199fdd1468a1569309d54304f4f558f9
Shubham Gawai <gawaishubham001@gmail.com>
2022-10-30 16:04:40 +0530

    403-01 done

---
+++ 4-Advance-JS/403-event-listeners/AdvanceJS-403-01/app.js
@@ -0,0 +1,12 @@
+
+document.getElementById("para").style.display = "none";
+
+function inputKey(x) {
+  let pref1 = document.getElementById("pid1");
+  pref1.innerHTML = `You Pressed <span>${x.key}</span>`;
+
+  document.getElementById("para").style.display = "block";
+
+  let pref2 = document.getElementById("para");
+  pref2.innerHTML = `<span class="new-span">${x.keyCode}</span>`;
+}
