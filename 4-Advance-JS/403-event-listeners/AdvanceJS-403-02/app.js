a0af0a54046dc6c3007a507bd2e40922fbb68de4
Shubham Gawai <gawaishubham001@gmail.com>
2022-10-30 15:16:18 +0530

    403-02 done

--- 4-Advance-JS/403-event-listeners/AdvanceJS-403-02/app.js
+++ 4-Advance-JS/403-event-listeners/AdvanceJS-403-02/app.js
@@ -13,31 +13,31 @@ for(var i=0;i<document.querySelectorAll('.drum').length;i++){
     })
     function makesound(buttontext){
       if(buttontext=='w'){
-          var tom1=new Audio("sounds/tom-1.mp3");
+          var tom1=new Audio("sound/tom-1.mp3");
           tom1.play();
       }
       if (buttontext == "a") {
-        var tom2 = new Audio("sounds/tom-2.mp3");
+        var tom2 = new Audio("sound/tom-2.mp3");
         tom2.play();
       }
       if (buttontext == "s") {
-        var tom3 = new Audio("sounds/tom-3.mp3");
+        var tom3 = new Audio("sound/tom-3.mp3");
         tom3.play();
       }
       if (buttontext == "d") {
-        var tom4 = new Audio("sounds/tom-4.mp3");
+        var tom4 = new Audio("sound/tom-4.mp3");
         tom4.play();
       }
       if (buttontext == "j") {
-        var snare = new Audio("sounds/snare.mp3");
+        var snare = new Audio("sound/snare.mp3");
         snare.play();
       }
       if (buttontext == "k") {
-        var kick = new Audio("sounds/kick-bass.mp3");
+        var kick = new Audio("sound/kick-bass.mp3");
         kick.play();
       }
       if (buttontext == "l") {
-        var crash = new Audio("sounds/crash.mp3");
+        var crash = new Audio("sound/crash.mp3");
         crash.play();
       }
     }
