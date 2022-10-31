20b3a489f6182315934752fc0bff500d2c61c24e
Shubham Gawai <gawaishubham001@gmail.com>
2022-10-31 14:13:32 +0530

    308-01 done

---
+++ 3-CSS/308-CSS-Pseudo-Classes/CSS-308-01/app.js
@@ -0,0 +1,47 @@
+
+//  success button - green functions
+function changecolorsuccess(x) {
+    x.style.backgroundColor = "green";
+    x.style.color = "white";
+
+}
+function normalcolorsuccess(x) {
+    x.style.background = "white";
+    x.style.color = "green"; 
+}
+
+
+//  info button - blue functions
+function changecolorinfo(x) {
+    x.style.backgroundColor = "blue";
+    x.style.color = "white";
+
+}
+function normalcolorinfo(x) {
+    x.style.background = "white";
+    x.style.color = "blue"; 
+}
+
+// warning button - orange functions
+function changecolorwarning(x) {
+    x.style.backgroundColor = "orange";
+    x.style.color = "white";
+
+}
+function normalcolorwarning(x) {
+    x.style.background = "white";
+    x.style.color = "orange"; 
+}
+
+// danger button - red functions
+function changecolordanger(x) {
+    x.style.backgroundColor = "red";
+    x.style.color = "white";
+
+}
+function normalcolordanger(x) {
+    x.style.background = "white";
+    x.style.color = "red"; 
+}
+
+  
