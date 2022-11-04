c4f013931a267e2d5fdf5a9f376ba36a72f3e26b
Shubham Gawai <gawaishubham001@gmail.com>
2022-11-04 14:51:29 +0530

    409-01 done

--- 4-Advance-JS/409-form-validation/AdvanceJS-409-01/app.js
+++ 4-Advance-JS/409-form-validation/AdvanceJS-409-01/app.js
@@ -26,64 +26,64 @@ function validation(e){
       
       if(first_namecheck.test(first_name)){
           document.getElementById('firsterror').innerHTML ="";
-          firstColor.style.border = "1.6px solid green";
+          firstColor.style.border = "3px solid green";
       }
       else {
           document.getElementById('firsterror').innerHTML =
           "First name must contain alphabets 3-18 characters";
-          firstColor.style.border = "1.6px solid red";
+          firstColor.style.border = "3px solid red";
       }
       if (last_namecheck.test(last_name)) {
         document.getElementById("lasterror").innerHTML = "";
-        lastColor.style.border = "1.6px solid green";
+        lastColor.style.border = "3px solid green";
       } else {
         document.getElementById("lasterror").innerHTML =
           "Last name must contain alphabets 3-18 characters";
-          lastColor.style.border = "1.6px solid red";
+          lastColor.style.border = "3px solid red";
       }
        if (emailcheck.test(email)) {
          document.getElementById("emailerror").innerHTML = "";
-         emailColor.style.border = "1.6px solid green";
+         emailColor.style.border = "3px solid green";
        } else {
          document.getElementById("emailerror").innerHTML =
            "Enter valid email, e.g. example@example.com";
-           emailColor.style.border = "1.6px solid red";
+           emailColor.style.border = "3px solid red";
        }
 
        if (passwordcheck.test(password)) {
          document.getElementById("passerror").innerHTML = "";
-         passwordColor.style.border = "1.6px solid green";
+         passwordColor.style.border = "3px solid green";
        } else {
          document.getElementById("passerror").innerHTML = 
          "Enter a valid strong password length 6-20"
          // "Password must contain atleast 1 uppercase,lowercase,digit,special char (length 6-20)";
-         passwordColor.style.border = "1.6px solid red";
+         passwordColor.style.border = "3px solid red";
        }
 
        if(telephonecheck.test(telephone)){
         document.getElementById("telephoneerror").innerHTML="";
-        telephoneColor.style.border = "1.6px solid green";
+        telephoneColor.style.border = "3px solid green";
        }
        else {
         document.getElementById("telephoneerror").innerHTML = 
         "Enter a valid telephone number";
-        telephoneColor.style.border = "1.6px solid red";
+        telephoneColor.style.border = "3px solid red";
        }
 
 
        if(biocheck.test(bio)){
         document.getElementById("bioerror").innerHTML="";
-        bioColor.style.border = "1.6px solid green";
+        bioColor.style.border = "3px solid green";
        }
        else {
         document.getElementById("bioerror").innerHTML = 
         "bio must contain lowercase  8-50 characters";
-        bioColor.style.border = "1.6px solid red";
+        bioColor.style.border = "3px solid red";
        }
 
 
 // if all the fields are correct means border for all input fields are green then the submit button should turn green in backgroung(success) :
-        if((firstColor.style.border == "1.6px solid green") && (lastColor.style.border == "1.6px solid green") && (emailColor.style.border == "1.6px solid green") && (passwordColor.style.border == "1.6px solid green") && (telephoneColor.style.border == "1.6px solid green") && ( bioColor.style.border == "1.6px solid green")){
+        if((firstColor.style.border == "3px solid green") && (lastColor.style.border == "3px solid green") && (emailColor.style.border == "3px solid green") && (passwordColor.style.border == "3px solid green") && (telephoneColor.style.border == "3px solid green") && ( bioColor.style.border == "3px solid green")){
         
           submitColor.style.backgroundColor = "darkgreen";
         } 
