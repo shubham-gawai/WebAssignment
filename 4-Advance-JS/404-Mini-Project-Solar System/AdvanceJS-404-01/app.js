583cdbaa13e7deb35eef99c5e5d533b885c5049e
Shubham Gawai <gawaishubham001@gmail.com>
2022-11-05 18:56:35 +0530

    solar project done

---
+++ 4-Advance-JS/404-Mini-Project-Solar System/AdvanceJS-404-01/app.js
@@ -0,0 +1,127 @@
+//
+document.getElementById("calculateF").style.display = "none";
+
+let calculateWeight = () => {
+
+        var nameOfPlanet = document.getElementById("nameOfPlanet");
+        var planetImage = document.getElementById("planetImage");
+        var mass = document.getElementById("mass");
+        var weight = document.getElementById("weight");
+        var dropd = document.getElementById("dropd");
+
+  if (mass.value === "" || dropd.value === "selectPlanet") {
+    if (mass.value === "") {
+      // alert("Please enter the value of Mass");
+      document.getElementById("calculateF").style.display = "none";
+      document.getElementById("default").style.display = "none";
+      document.getElementById("required-msg1").style.display = "block";
+      document.getElementById("required-msg2").style.display = "none";
+
+      return;
+    } else if (dropd.value === "selectPlanet") {
+      // alert("Please Select Planet");
+      document.getElementById("calculateF").style.display = "none";
+      document.getElementById("default").style.display = "none";
+      document.getElementById("required-msg1").style.display = "none";
+      document.getElementById("required-msg2").style.display = "block";
+
+      return;
+    }
+  } else {
+    document.getElementById("calculateF").style.display = "block";
+
+    if (dropd.value === "mercury") {
+      document.getElementById("default").style.display = "none";
+
+      document.getElementById("required-msg1").style.display = "none";
+      document.getElementById("required-msg2").style.display = "none";
+
+      nameOfPlanet.innerText = "MERCURY";
+      planetImage.src = "ProjectImage/mercury.png";
+      weight.innerText = `${parseFloat(mass.value * 3.59).toFixed(2)} N`;
+    } else if (dropd.value === "venus") {
+      document.getElementById("default").style.display = "none";
+
+      document.getElementById("required-msg1").style.display = "none";
+      document.getElementById("required-msg2").style.display = "none";
+
+      nameOfPlanet.innerText = "VENUS";
+      planetImage.src = "ProjectImage/venus.png";
+      weight.innerText = `${parseFloat(mass.value * 8.87).toFixed(2)} N`;
+    } else if (dropd.value === "earth") {
+      document.getElementById("default").style.display = "none";
+
+      document.getElementById("required-msg1").style.display = "none";
+      document.getElementById("required-msg2").style.display = "none";
+
+      nameOfPlanet.innerText = "EARTH";
+      planetImage.src = "ProjectImage/earth.png";
+      weight.innerText = `${parseFloat(mass.value * 9.81).toFixed(2)} N`;
+    } else if (dropd.value === "moon") {
+      document.getElementById("default").style.display = "none";
+
+      document.getElementById("required-msg1").style.display = "none";
+      document.getElementById("required-msg2").style.display = "none";
+
+      nameOfPlanet.innerText = "MOON";
+      planetImage.src = "ProjectImage/moon.png";
+      weight.innerText = `${parseFloat(mass.value * 1.62).toFixed(2)} N`;
+    } else if (dropd.value === "mars") {
+      document.getElementById("default").style.display = "none";
+
+      document.getElementById("required-msg1").style.display = "none";
+      document.getElementById("required-msg2").style.display = "none";
+
+      nameOfPlanet.innerText = "MARS";
+      planetImage.src = "ProjectImage/mars.png";
+      weight.innerText = `${parseFloat(mass.value * 3.77).toFixed(2)} N`;
+    } else if (dropd.value === "jupiter") {
+      document.getElementById("default").style.display = "none";
+
+      document.getElementById("required-msg1").style.display = "none";
+      document.getElementById("required-msg2").style.display = "none";
+
+      nameOfPlanet.innerText = "JUPITER";
+      planetImage.src = "ProjectImage/jupiter.png";
+      weight.innerText = `${parseFloat(mass.value * 25.95).toFixed(2)} N`;
+    } else if (dropd.value === "saturn") {
+      document.getElementById("default").style.display = "none";
+
+      document.getElementById("required-msg1").style.display = "none";
+      document.getElementById("required-msg2").style.display = "none";
+
+      nameOfPlanet.innerText = "SATURN";
+      planetImage.src = "ProjectImage/saturn.png";
+      weight.innerText = `${parseFloat(mass.value * 11.08).toFixed(2)} N`;
+    } else if (dropd.value === "uranus") {
+      document.getElementById("default").style.display = "none";
+
+      document.getElementById("required-msg1").style.display = "none";
+      document.getElementById("required-msg2").style.display = "none";
+
+      nameOfPlanet.innerText = "URANUS";
+      planetImage.src = "ProjectImage/uranus.png";
+      weight.innerText = `${parseFloat(mass.value * 10.67).toFixed(2)} N`;
+    } else if (dropd.value === "neptune") {
+      document.getElementById("default").style.display = "none";
+
+      document.getElementById("required-msg1").style.display = "none";
+      document.getElementById("required-msg2").style.display = "none";
+
+      nameOfPlanet.innerText = "NEPTUNE";
+      planetImage.src = "ProjectImage/neptune.png";
+      weight.innerText = `${parseFloat(mass.value * 14.07).toFixed(2)} N`;
+    } else if (dropd.value === "pluto") {
+      document.getElementById("default").style.display = "none";
+
+      document.getElementById("required-msg1").style.display = "none";
+      document.getElementById("required-msg2").style.display = "none";
+
+      nameOfPlanet.innerText = "PLUTO";
+      planetImage.src = "ProjectImage/pluto.png";
+      weight.innerText = `${parseFloat(mass.value * 0.42).toFixed(2)} N`;
+    }
+    dropd.value = "selectPlanet";
+    mass.value = "";
+  }
+};
---
+++ 4-Advance-JS/404-Mini-Project-Solar System/Readme.md
@@ -0,0 +1,23 @@
+<hr>
+
+Dear Geeks!👋 
+
+We will now complete a few quick mini-projects to recap the topics we have already covered.
+You should try to complete this project since it will help you better comprehend HTML, CSS, Javascript, and concepts like event listeners and dom manipulation.
+
+So, don't be afraid to try it.
+
+<hr>
+
+## Exercises AdvanceJS-404-01
+
+
+1. Develop a small application which calculate a weight of an object in a certain planet. 
+
+![Solar System](./image/dom_min_project_solar_system_day_4.1.gif)
+
+# Steps to submit your assignments
+
+- Complete the assignments listed above in respective folders.
+- Push the assignments.
+- Add the link in ```./../README.md``` 
