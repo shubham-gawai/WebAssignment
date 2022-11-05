9de921a473b075fc76086c1c9895780499949448
Shubham Gawai <gawaishubham001@gmail.com>
2022-11-05 20:14:32 +0530

    default image not showing

--- 4-Advance-JS/404-Mini-Project-Solar System/AdvanceJS-404-01/app.js
+++ 4-Advance-JS/404-Mini-Project-Solar System/AdvanceJS-404-01/app.js
@@ -55,7 +55,7 @@ let calculateWeight = () => {
       document.getElementById("required-msg2").style.display = "none";
 
       nameOfPlanet.innerText = "EARTH";
-      planetImage.src = "earth.png";
+      planetImage.src = "ProjectImage/earth.png";
       weight.innerText = `${parseFloat(mass.value * 9.81).toFixed(2)} N`;
     } else if (dropd.value === "moon") {
       document.getElementById("default").style.display = "none";
