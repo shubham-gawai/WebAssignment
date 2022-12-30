550838b361963274c806a7002041d23285ad1488
Shubham Gawai <gawaishubham001@gmail.com>
2022-12-30 13:37:16 +0530

    401-03 done

--- 4-Advance-JS/401-DOM-Manipulation/AdvanceJS-401-03/app.js
+++ 4-Advance-JS/401-DOM-Manipulation/AdvanceJS-401-03/app.js
@@ -1,26 +1,29 @@
-// 
-let count=1;
+// alert("working");
+let count = 1;
 
-function controller(x){
-    count+=x;
-    corousel(count);
+function controller(x) {
+  count += x;
+  corousel(count);
 }
 
 corousel(count);
 
-function corousel(num){
-    let slides=document.getElementsByClassName('slide');
-    if (num == slides.length){
-        num=0;
-        count=0;
-    }
-    if(num==-1){
-        num=slides.length-1;
-        count=slides.length - 1;
-    }
-      //console.log(slides);
-      for (let y of slides) {
-        y.style.display = "none";
-      }
-    slides[num].style.display="block"
+function corousel(num) {
+  let slides = document.getElementsByClassName("slide");
+
+  if (num == slides.length) {
+    num = 0;
+    count = 0;
+  }
+
+  if (num == -1) {
+    num = slides.length - 1;
+    count = slides.length - 1;
+  }
+  //console.log(slides);
+  
+  for (let y of slides) {
+    y.style.display = "none";
+  }
+  slides[num].style.display = "block";
 }
